de3e539a01661772d80a2ed02d5e94f861f4f683
Wing Hei Chan <whmunkchan@outlook.com>
2023-01-20 01:12:37 +0800

    ; Remove unused dependencies
    * gref-doc/info.rkt:
    RnRS dependencies no longer needed due to 3ca557c.

--- gref-doc/info.rkt
+++ gref-doc/info.rkt
@@ -11,8 +11,7 @@
 (define build-deps
   '("scribble-lib"
     "gref-lib" "srfi-lib"
-    "algol60" "glass"
-    "lens-doc" "r5rs-doc" "r6rs-doc" "racket-doc" "srfi-doc"))
+    "algol60" "glass" "lens-doc" "racket-doc" "srfi-doc"))
 (define pkg-authors '("whmunkchan@outlook.com"))
 (define pkg-desc "Documentation part of \"gref\"")
 (define license 'GPL-3.0-or-later)
