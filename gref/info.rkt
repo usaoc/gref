f8b84ec89ad021476a7a59a58a72045644a29e92
Wing Hei Chan <whmunkchan@outlook.com>
2023-01-19 17:47:06 +0800

    ; Add meta package
    * .gitignore:
    * gref/COPYING:
    * gref/info.rkt:

---
+++ gref/info.rkt
@@ -0,0 +1,14 @@
+#lang info
+;; Copyright (C) 2022 Wing Hei Chan
+
+;; Copying and distribution of this file, with or without
+;; modification, are permitted in any medium without royalty provided
+;; the copyright notice and this notice are preserved.  This file is
+;; offered as-is, without any warranty.
+
+(define collection 'multi)
+(define deps '("gref-doc" "gref-lib"))
+(define implies deps)
+(define pkg-authors '("whmunkchan@outlook.com"))
+(define pkg-desc "Generalized references for Racket")
+(define license '(GPL-3.0-or-later))
